0b7fbc5d2a2f6195a8c32ca9112a4a96c0148231
tbostick <timbostick1950@gmail.com>
2019-01-31 13:19:09 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+
+        //  Code to print numbers from 10 to 1
+        for (int i = 10; i > 0; i--)
+        {
+            System.out.println(i);
+            //End of the code
+        }
+    }
+}
